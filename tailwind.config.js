ec986dbd4a169f6811f7683c09a1735df6303a25
GoldenQ12 <goldenytstorm@gmail.com>
2025-05-21 10:58:41 +0200

    Last Commit, getting rid of the DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-    "./node_modules/daisyui/**/*.js"     
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("daisyui"),
     require("tailwindcss-animated"),
   ],
   daisyui: {
