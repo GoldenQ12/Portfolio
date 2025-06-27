f1fae5c1d79644e0a16f1b83e80c4ed5a357cdbf
GOLDEN\golde <alexander.sal.roj@gmail.com>
2025-06-27 12:54:47 +0200

    Data Service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,28 +9,12 @@ import { BehaviorSubject, map, Observable } from 'rxjs';
 export class DataService {
 
 
-  private currentLanguage = new BehaviorSubject<string>('en');
-  currentLanguage$ = this.currentLanguage.asObservable();
-
   constructor(
     private http: HttpClient,
   ) { }
 
   getNavBar(){
     return this.http.get<any[]>('/assets/data/menus.json');
-  }
-
-  getTranslations(lang: string): Observable<{[key: string]: string}> {
-    return this.http.get<any>('/assets/data/translations.json').pipe(
-      map(data => {
-        const translations = data[lang][0];
-        return translations;
-      })
-    );
-  }
-
-  changeLanguage(lang: string) {
-    this.currentLanguage.next(lang);
   }
 
 
